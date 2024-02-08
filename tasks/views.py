3ca130fb0bb043b730685cabddff78a8309c5667
Rafaelmarinr <rafaelmarinantonior@gmail.com>
2024-02-07 22:36:00 -0400

    Complite and delete

--- tasks/views.py
+++ tasks/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
 # Create your views here.
 
 
@@ -83,6 +84,19 @@ def task_detail(request, task_id):
                 'form': form,
                 'error': 'Error Updating Task'
             })
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user = request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user = request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
 
 def signout(request):
     logout(request)
